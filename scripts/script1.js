2ac5dfbe83daf08d81056e104174a7a31b04042f
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2025-02-21 20:58:06 -0800

    formulario

---
+++ scripts/script1.js
@@ -0,0 +1,31 @@
+// keyword variableName = value;
+// var, let, const
+
+//String
+var firstName = "Samantha";
+var lastName = "Jimenez";
+
+//concatenation
+console.log("Name: "+firstName + " " + lastName);
+
+firstName = "Fernanda";// cambia
+
+//numerical
+let myAge=99;
+let myHeight=100;
+
+console.log(myAge,myHeight);
+
+//boolean (true/false)
+let isStudent = true; 
+
+//math operations + - / *
+let num1=10;
+let num2=12;
+
+document.write("<p>El resultado es = " + (num1+num2) + "</p>");
+document.write("El resultado es = " + (num1-num2));
+document.write("El resultado es = " + (num1*num2));
+document.write("El resultado es = " + (num1/num2).toFixed(3));
+
+//alert("Esto es un mensaje de alerta");
